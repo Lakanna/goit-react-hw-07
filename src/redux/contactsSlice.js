2b3c76a5c3c25905c5e9aba8831ecebf163dd87b
Lakanna <134102704+Lakanna@users.noreply.github.com>
2024-09-01 20:32:49 +0100

    Initial commit

---
+++ src/redux/contactsSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const contactsSlice = createSlice({
+  name: "contacts",
+  initialState: {
+    items: [
+      { id: 1, name: "Frodo Hopkins", number: "911-11-11" },
+      { id: 2, name: "Gandalf", number: "333-33-33" },
+    ],
+  },
+  reducers: {
+    addContact: (state, action) => {
+      return { ...state, items: [...state.items, action.payload] };
+    },
+    deleteContact: (state, action) => {
+      return {
+        ...state,
+        items: state.items.filter((contact) => contact.id !== action.payload),
+      };
+    },
+  },
+});
+
+export const selectContacts = (state) => state.contacts.items;
+
+export const { addContact, deleteContact } = contactsSlice.actions;
+
+export default contactsSlice.reducer;
